6750f18700334aa2e4861f5d79eefc93baf62af1
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-08-29 15:30:27 +0200

    Add full Express structure

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+// Setup (configuration of your app)
+// -----------------------------------------------------------------------------
 const express = require("express");
 
 // "app" variable represents our application
 // (all configuration and content will be defined through "app")
 const app = express();
 
+// Make Express aware of the "public/" folder
+// (makes all files inside "public/" available through localhost:3000)
+app.use(express.static("public"));
 
+
+
+// Routes (content of your app)
+// -----------------------------------------------------------------------------
 // Express Route - defines a page of content on your website
 app.get("/", (request, response, next) => {
-  response.send("Hello, world!");
+  response.sendFile(__dirname + "/views/home-page.html");
+    // __dirname = "DIRECTORY NAME"
+});
+
+app.get("/cat", (request, response, next) => {
+  response.sendFile(__dirname + "/views/cat-page.html");
+    // __dirname = "DIRECTORY NAME"
 });
 
 
